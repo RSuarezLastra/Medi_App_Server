62a59ae63261617670cc6c150097ffb52cbc1ef3
RSLastra <raul.slastra@gmail.com>
2023-10-26 14:44:50 -0600

    ruta de registro usuario

--- src/db.js
+++ src/db.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-    DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const pg = require("pg");
+const { DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/mediapp`, {
+const sequelize = new Sequelize( DATABASE_URL, {
     logging: false,
     native: false,
+    dialectModule: pg,
 });
 
 const basename = path.basename(__filename);

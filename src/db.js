9b34e7a06958af8c68f0a34a541619038cf6a0b4
diegoalparra <diegoalparra08@gmail.com>
2023-10-26 15:48:40 -0500

    server

--- src/db.js
+++ src/db.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
+const pg = require("pg")
 const path = require('path');
-const {
-    DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/mediapp`, {
+const { DATABASE_URL } = process.env;
+
+const sequelize = new Sequelize(DATABASE_URL, {
     logging: false,
     native: false,
+    dialectModule: pg,
 });
 
 const basename = path.basename(__filename);

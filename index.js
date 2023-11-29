d8ab8797a19eda3945d1142e31c8b913f104a002
RSLastra <raul.slastra@gmail.com>
2023-11-28 18:14:12 -0600

    agregando vercel.json

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require('./src/index.js')
 const { conn } = require('./src/db.js')
 
+const PORT = process.env.PORT || 3001;
+
 conn.sync({ alter: true }).then(() => {
-    server.listen(3001, () => {
+    server.listen(PORT, () => {
         console.log('%s listening at 3001');
     })
 })
